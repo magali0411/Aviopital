c6d8bcad34d95eab69fa4f61e60d3a7144ac3f6d
magali <magal@LAPTOP-Magali.lan>
2020-04-12 14:32:12 +0200

    création d'une factory (dao pattern)

---
+++ src/dao/Factory.java
@@ -0,0 +1,46 @@
+package dao;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+
+
+public class Factory implements Serializable{
+		
+	private static Factory instance = null;
+	private Connection connection = null;
+	
+	private String url;
+
+
+	private static final Logger log = Logger.getLogger("Application");
+
+	private Factory() {
+
+			try {
+				Class.forName("org.sqlite.JDBC");
+				url = "jdbc:sqlite:c:/Developpement/escrim.db";
+				connection = DriverManager.getConnection(url); // chemin vers la base de donnes
+			} catch (Exception e) {
+				System.err.println(e.getClass().getName() + ": " + e.getMessage());
+				System.exit(0);
+			}
+			
+			System.out.println("Opened database successfully");
+			log.info("Opened database successfully");
+
+			//createDatabase();
+			
+		}
+
+		public static Factory getInstance() {
+			if (instance == null) {
+				instance = new Factory();
+			}
+			return instance;
+		}
+}
+
