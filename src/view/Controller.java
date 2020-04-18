20d171647790fc1b998d8eccb568a3ba3eacc2a4
Eliyahou <eliyahou.gabbay@mines-ales.org>
2020-04-18 13:33:18 +0200

    First Commit for the view

---
+++ src/view/Controller.java
@@ -0,0 +1,47 @@
+package view;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
+public class Controller implements EventHandler {
+
+
+    @Override
+    public void handle(Event event) {
+        // confirm button
+        if (((Button) event.getSource()).getText() == "Confirm") {
+            if (event.getEventType() == MouseEvent.MOUSE_ENTERED)
+                ((Button) event.getSource()).setStyle("-fx-background-color: #00DA63; " +
+                        "-fx-font: bold 2.1em \"Arial\";" +
+                        "-fx-text-fill: #ffffff;" +
+                        "-fx-background-radius: 90;");
+            if (event.getEventType() == MouseEvent.MOUSE_EXITED)
+                ((Button) event.getSource()).setStyle("-fx-background-color: #00DA63; " +
+                        "-fx-font: bold 2em \"Arial\";" +
+                        "-fx-text-fill: #ffffff;" +
+                        "-fx-background-radius: 90;");
+        }
+        if (((Button) event.getSource()).getText() == "Export Excel") {
+            if (event.getEventType() == MouseEvent.MOUSE_ENTERED)
+                ((Button) event.getSource()).setStyle("-fx-background-color: #262626; " +
+                        "-fx-background-radius: 90;"+
+                        "-fx-font: bold 1.7em \"Arial\";" +
+                        "-fx-border-width: 2;" +
+                        "-fx-text-fill: #ffffff;" +
+                        "-fx-border-style: solid;" +
+                        "-fx-border-color: #ffffff;" +
+                        "-fx-border-radius: 90;");
+            if (event.getEventType() == MouseEvent.MOUSE_EXITED)
+                ((Button) event.getSource()).setStyle("-fx-background-color: #262626; " +
+                        "-fx-background-radius: 90;"+
+                        "-fx-font: bold 1.5em \"Arial\";" +
+                        "-fx-border-width: 2;" +
+                        "-fx-text-fill: #ffffff;" +
+                        "-fx-border-style: solid;" +
+                        "-fx-border-color: #ffffff;" +
+                        "-fx-border-radius: 90;");
+        }
+    }
+}
