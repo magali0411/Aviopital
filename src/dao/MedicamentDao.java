edd22fba16cf7ef70f031515172ee27c32425d8c
magali <magal@LAPTOP-Magali.lan>
2020-04-19 14:09:06 +0200

    Ajout de commentaires selon les normes de la javadoc

--- src/dao/MedicamentDao.java
+++ src/dao/MedicamentDao.java
@@ -4,8 +4,17 @@ import app.hopital.Medicament;
 
 public interface MedicamentDao {
 	
+	/**
+	 * Méthode pour créer un médicament dans la base de données
+	 * @param medicament
+	 */
 	void create( Medicament medicament );
 
+	/**
+	 * Méthode pour trouver un médicament selon son ID 
+	 * @param id
+	 * @return
+	 */
 	Medicament find( String id );
 
 }
