3414bb014bd87d070d33f4dbcbc41c484e9a5e1e
Eliyahou <eliyahou.gabbay@mines-ales.org>
2020-04-19 18:53:35 +0200

    Modification todolist

--- src/view/View2.java
+++ src/view/View2.java
@@ -1,8 +1,12 @@
 package view;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -17,15 +21,18 @@ public class View2 implements IView {
     public View2() {
     }
 
+    // -------------------------- TOOLS BAR -------------------------
 
+    // Tools bar : specify plane, Add element, Export Excel, Import Excel
     public static VBox addTools() {
         VBox toolsBox = new VBox();
+        toolsBox.setPadding(new Insets(5, 10, 5, 0));
 
-        // Specify plane
-        Button definePlane = new Button("Specify Plane");
+        // Tools label
+        Label tools = new Label("Tools");
 
         // Order
-        Button orderButton = new Button("Add Stuff");
+        Button orderButton = new Button("Add Element");
 
         // Export Excel
         Button exportExcelButton = new Button("Export Excel");
@@ -33,12 +40,15 @@ public class View2 implements IView {
         // Import Excel
         Button importExcelButton = new Button("Import Excel");
 
-        toolsBox.getChildren().addAll(definePlane, orderButton, exportExcelButton, importExcelButton);
+        toolsBox.getChildren().addAll(tools, orderButton, exportExcelButton, importExcelButton);
 
         return toolsBox;
     }
 
 
+    // -------------------------- PLANE SPECIFICATION-------------------------
+
+    // Plane specification
     public static GridPane addPlaneData() {
 
         // Set the grid that contains all the plane information
@@ -46,10 +56,17 @@ public class View2 implements IView {
         planeGrid.setGridLinesVisible(true);
         planeGrid.setHgap(10);
         planeGrid.setVgap(10);
+        planeGrid.setPadding(new Insets(5, 0, 5, 10));
 
         // Plane name
-        Label planeLabel = new Label("Antonov An 12");
-        planeGrid.add(planeLabel, 0, 0, 3, 1);
+        ObservableList<String> planes = FXCollections.observableArrayList(
+                "Antonov An 12",
+                "BAe 146"
+        );
+        final ComboBox planeSelection = new ComboBox(planes);
+        planeSelection.setValue(planes.get(0));
+        //Label planeLabel = new Label("Antonov An 12");
+        planeGrid.add(planeSelection, 0, 0, 3, 1);
 
         // Door dimension column 1-2, rows 3
         Label doorDimension = new Label("Door dimensions (cm)");
@@ -92,18 +109,102 @@ public class View2 implements IView {
         return planeGrid;
     }
 
+    // -------------------------- ELEMENTS PICKING -------------------------
 
-    public static BorderPane addMainBorderPane() {
-        BorderPane centerPane = new BorderPane();
+    // AddElement bar has 2 button one to set the hospital, the other to set the colis
+    public static HBox addAddElementBar() {
+        HBox elements = new HBox();
 
-        return centerPane;
+        // Hospital element
+        Button hospitalButton = new Button("Hospital");
+
+        // Colis element
+        Button colisButton = new Button("Colis");
+
+        elements.getChildren().addAll(hospitalButton, colisButton);
+
+        return elements;
     }
 
-    public static GridPane addSetEscrim(){
-        GridPane escrim = new GridPane();
+    // The two following pane describe the hospital Pane and the other one the colis pane
+    public static GridPane addHospitalPane() {
+        GridPane hospitalPane = new GridPane();
 
-        return  escrim;
+        return hospitalPane;
     }
+
+    public static GridPane addColisPane() {
+
+        GridPane colisPane = new GridPane();
+        colisPane.setGridLinesVisible(true);
+        colisPane.setVgap(10);
+        colisPane.setHgap(10);
+
+        /*affectataire
+         * module
+         *secteur varchar(20)
+         *  nature_colis  varchar(20)
+         * num_colis int(5)
+         * designation_colis varchar(30)
+         * precision_articles varchar(50)
+         * dim varchar(20) LxWxH
+         * volume float(5)
+         * poids int(5)
+         * observations varchar(20)
+         */
+
+        // Affectataire
+        Label affectataire = new Label("Affectataire: " + "DAC");
+        colisPane.add(affectataire, 0, 0, 2, 1);
+
+        Label module = new Label("Module: " + "Comp. E2");
+        colisPane.add(module, 0, 1, 2, 1);
+
+        Label secteur = new Label("Secteur: " + "(L) Stock logistique");
+        colisPane.add(secteur, 0, 2, 2, 1);
+
+        Label nature = new Label("Nature: " + "Bac");
+        colisPane.add(nature, 0, 3, 2, 1);
+
+        Label num_colis = new Label("num: " + "17");
+        colisPane.add(num_colis, 0, 4, 2, 1);
+
+        Label designationAndPrecision = new Label("Soutien logistique : echaffaudage + 3 kits elec tm36/E2 + epsac");
+        colisPane.add(designationAndPrecision, 0, 5, 2, 1);
+
+        Label dimensionLabel = new Label("Dimension: " + "120x80x100");
+        colisPane.add(dimensionLabel, 0, 6, 2, 1);
+
+        Label volumeTitle = new Label("Volume: ");
+        Label volume = new Label("0.96");
+        colisPane.add(volumeTitle, 0, 7);
+        colisPane.add(volume, 0, 8);
+
+        Label poidsTitle = new Label("Poids: ");
+        Label poids = new Label("112");
+        colisPane.add(poidsTitle, 1, 7);
+        colisPane.add(poids, 1, 8);
+
+        Label observationTitle = new Label("Observations: " + "à compléter");
+        colisPane.add(observationTitle, 0, 9, 2, 1);
+
+        return colisPane;
+    }
+
+    public static BorderPane addAddElementPane() {
+        BorderPane mainPane = new BorderPane();
+
+        // AddElement tool bar with hospital & colis tab
+        mainPane.setTop(addAddElementBar());
+
+        // Add by default the hospital pane, will be changed after
+        mainPane.setCenter(addColisPane());
+
+        return mainPane;
+    }
+
+
+    // --------------------------- SCENE --------------------------------------
 
     public static Scene createFirstScene(Group root, Controller view_controller) {
 
@@ -115,6 +216,8 @@ public class View2 implements IView {
         BorderPane borderPane = new BorderPane();
         // use method : setTop, setRight to set the pane border
         borderPane.setLeft(addTools());
+
+        borderPane.setCenter(addAddElementPane());
 
         borderPane.setRight(addPlaneData());
 
